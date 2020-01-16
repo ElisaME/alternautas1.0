f8606a15a5fa86b7c5a6e818cc79fb2662ae1350
Elisa <elisam.esquivel@gmail.com>
2020-01-16 13:05:59 -0600

    Structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="App">
       <p>Holaaaaaa Kiffy</p>
+      <Menu
     </div>
   );
 }
